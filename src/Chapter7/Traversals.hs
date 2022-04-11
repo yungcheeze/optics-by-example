6e061c7d177172624df9974fff608891ca1f9aac
Ucizi Mafeni <ucizim@gmail.com>
2022-04-11 16:06:53 +0100

    ch7.3 finish off exercises

--- src/Chapter7/Traversals.hs
+++ src/Chapter7/Traversals.hs
@@ -22,12 +22,60 @@ q1' = ("Jurassic", "Park") & both .~ "N/A"
 
 q1'' = ("Jurassic", "Park") & each .~ "N/A"
 
+{-
+  ("N/A", "N/A")
+-}
+
 q2 = ("Jurassic", "Park") & both . traversed .~ 'x'
+
+{-
+  ("xxxxxxxx", "xxxx")
+-}
 
 q3 =
   ("Malcolm", ["Kaylee", "Inara", "Jayne"])
     & beside id traversed %~ take 3
 
-q4 = ["Die Another Day", "Live and Let Die", "You Only Live Twice"] & traversed . elementOf worded 1 . traversed .~ 'x'
+{-
+  ("Mal", ["Kay", "Ina", "Jay"])
+-}
 
-q5 = ((1, 2), (3, 4)) & beside both both +~ 1
+q4 = ("Malcolm", ["Kaylee", "Inara", "Jayne"]) & _2 . element 1 .~ "River"
+
+q4' = ("Malcolm", ["Kaylee", "Inara", "Jayne"]) & elementOf (beside id traversed) 2 .~ "River"
+
+{-
+  ("Malcolm", ["Kaylee", "River", "Jayne"])
+-}
+
+q5 = ["Die Another Day", "Live and Let Die", "You Only Live Twice"] & traversed . elementOf worded 1 . traversed .~ 'x'
+
+{-
+  [ "Die xxxxxxx Day" , "Live xxx Let Die" , "You xxxx Live Twice" ]
+-}
+
+q6 = ((1, 2), (3, 4)) & beside both both +~ 1
+
+{-
+  ((2, 3), (4, 5))
+-}
+
+q7 = (1, (2, [3, 4])) & beside id (beside id each) +~ 1
+
+{-
+  (2, (3, [4, 5]))
+-}
+
+q8 = ((True, "Strawberries"), (False, "Blueberries"), (True, "Blackberries")) & each . filteredBy (_1 . filtered id) . _2 . taking 5 traversed %~ toUpper
+
+q8' = ((True, "Strawberries"), (False, "Blueberries"), (True, "Blackberries")) & each . filtered fst . _2 . taking 5 traversed %~ toUpper
+
+{-
+  ((True, "STRAWberries"), (False, "Blueberries"), (True, "BLACKberries"))
+-}
+
+q9 = ((True, "Strawberries"), (False, "Blueberries"), (True, "Blackberries")) & each %~ snd
+
+{-
+  ("Strawberries", "Blueberries", "Blackberries")
+-}
